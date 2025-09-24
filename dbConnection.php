7750db76d0e0fc72ba8ddbca7218a5e87acc7303
185august <augusteide98@hotmail.com>
2025-09-24 11:43:22 +0200

    A php website to display content from a database. So far I have made two tables where one displays information about different events and the other displays information about the different tickets that are available

---
+++ dbConnection.php
@@ -0,0 +1,15 @@
+<?php
+
+function getDbConnection() {
+$dbHost = getenv('DB_HOST');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
+$connection = pg_connect("host=$dbHost dbname=$dbName user=$dbUser password=$dbPass");
+if(!$connection){
+    echo "Connection failed";
+    exit;
+}
+return $connection;
+}
+
