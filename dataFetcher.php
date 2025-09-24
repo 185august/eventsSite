4eab660837de10e857a5a28b178e59c2436a0ed1
185august <augusteide98@hotmail.com>
2025-09-24 13:13:03 +0200

    Seperated out much of the php code in `main.php` into `dataFetcher.php` to make the code less cluttered and maintainable

---
+++ dataFetcher.php
@@ -0,0 +1,63 @@
+<?php
+include 'dbConnection.php';
+$connection = getDbConnection();
+
+
+$eventsResult = pg_query($connection, "SELECT event_id, title, description,  to_char(start_time, 'YYYY-MM-DD HH24:MI') as start_time,
+           to_char(end_time, 'YYYY-MM-DD HH24:MI') as end_time, name FROM events
+    JOIN venues
+        ON events.venue_id = venues.venue_id");
+if (!$eventsResult) {
+    echo "Events query failed";
+    exit;
+}
+
+
+$ticketsResult = pg_query($connection, "SELECT ticket_id,title,
+           to_char(start_time, 'YYYY-MM-DD HH24:MI') as start_time,
+           to_char(end_time, 'YYYY-MM-DD HH24:MI') as end_time,
+           price,
+           available_quantity,
+           ticket_type
+    FROM tickets t
+    JOIN events e
+         ON t.event_id = e.event_id
+        JOIN ticket_types tt
+        on t.ticket_type_id = tt.ticket_type_id;");
+if (!$ticketsResult) {
+    echo "Tickets query failed";
+    exit;
+}
+
+$ordersResult = pg_query($connection, "SELECT oi.order_id,
+       name,
+       title,
+       quantity,
+       price_at_purchase,
+       status,
+       ticket_type
+    FROM order_items oi
+    JOIN orders o
+         ON  oi.order_id= o.order_id
+        JOIN tickets t
+        on oi.ticket_id = t.ticket_id
+        JOIN ticket_types tt
+        on t.ticket_type_id = tt.ticket_type_id
+        JOIN events e
+        on t.event_id = e.event_id
+        JOIN users u
+        on o.user_id = u.user_id
+        ");
+if (!$ordersResult) {
+    echo "Orders query failed";
+    exit;
+}
+
+
+pg_close($connection);
+
+return [
+    'events' => $eventsResult,
+    'tickets' => $ticketsResult,
+    'orders' => $ordersResult
+];
--- main.php
+++ main.php
@@ -3,41 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Events Data</title>
-
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<div class="main-content-wrapper">
-    <?php
-    include 'dbConnection.php';
-
-    $connection = getDbConnection();
-    $result = pg_query($connection, "SELECT event_id, title, description,  to_char(start_time, 'YYYY-MM-DD HH24:MI') as start_time,
-           to_char(end_time, 'YYYY-MM-DD HH24:MI') as end_time, name FROM events
-    JOIN venues
-        ON events.venue_id = venues.venue_id");
-    if (!$result) {
-        echo "Query failed";
-
-        exit;
-    }
-    ?>
-    <div class='event-container'>
-        <h1>Events</h1>
-        <table class="event-table">
-
-            <tr>
-                <th>Event Name</th>
-                <th>Event Description</th>
-                <th>Start time</th>
-                <th>End time</th>
-                <th>Venue</th>
-            </tr>
-
-            <?php
-            while ($row = pg_fetch_assoc($result)) {
-                echo "
+<?php
+$dbResults = include 'dataFetcher.php';
+$ticketsResult = $dbResults['tickets'];
+$ordersResult = $dbResults['orders'];
+$eventsResult = $dbResults['events'];
+?>
+<div class='event-container'>
+    <h1>Events</h1>
+    <table class="event-table">
+        <tr>
+            <th>Event Name</th>
+            <th>Event Description</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th>Venue</th>
+        </tr>
+        <?php
+        while ($row = pg_fetch_assoc($eventsResult)) {
+            echo "
                 <tr id='{$row['event_id']}' >
                 <td>{$row['title']}</td>
                 <td>{$row['description']}</td>
@@ -46,47 +33,25 @@
                 <td>{$row['name']}</td>
                 </tr>
                 ";
-            }
-            ?>
-        </table>
-    </div>
+        }
+        ?>
+    </table>
+</div>
+<div class='event-container'>
+    <h1>Tickets</h1>
+    <table class="event-table">
+        <tr>
+            <th>Event Name</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th>Ticket Type</th>
+            <th>Ticket price</th>
+            <th>Ticket availability</th>
+        </tr>
+        <?php
 
-
-    <?php
-
-    $result = pg_query($connection, "SELECT ticket_id,title,
-           to_char(start_time, 'YYYY-MM-DD HH24:MI') as start_time,
-           to_char(end_time, 'YYYY-MM-DD HH24:MI') as end_time,
-           price,
-           available_quantity,
-           ticket_type
-    FROM tickets t
-    JOIN events e
-         ON t.event_id = e.event_id
-        JOIN ticket_types tt
-        on t.ticket_type_id = tt.ticket_type_id;");
-    if (!$result) {
-        echo "Query failed";
-        exit;
-    }
-    ?>
-
-    <div class='event-container'>
-        <h1>Tickets</h1>
-        <table class="event-table">
-
-            <tr>
-                <th>Event Name</th>
-                <th>Start time</th>
-                <th>End time</th>
-                <th>Ticket Type</th>
-                <th>Ticket price</th>
-                <th>Ticket availability</th>
-            </tr>
-
-            <?php
-            while ($row = pg_fetch_assoc($result)) {
-                echo "
+        while ($row = pg_fetch_assoc($ticketsResult)) {
+            echo "
                 <tr id='{$row['ticket_id']}' >
                 <td>{$row['title']}</td>
                 <td>{$row['start_time']}</td>
@@ -96,13 +61,39 @@
                 <td>{$row['available_quantity']}</td>
                 </tr>
                 ";
-            }
-            pg_close($connection);
-            ?>
-        </table>
-    </div>
-
+        }
+        ?>
+    </table>
 </div>
-
+<div class='event-container'>
+    <h1>Orders</h1>
+    <table class="event-table">
+        <tr>
+            <th>Order number</th>
+            <th>User Name</th>
+            <th>Event Name</th>
+            <th>Quantity</th>
+            <th>Ticket price</th>
+            <th>Ticket Type</th>
+            <th>Status</th>
+        </tr>
+        <?php
+        // The $orders_result variable is populated by the included data_fetcher.php script.
+        while ($row = pg_fetch_assoc($ordersResult)) {
+            echo "
+                <tr id='{$row['order_id']}' >
+                <td>{$row['order_id']}</td>
+                <td>{$row['name']}</td>
+                <td>{$row['title']}</td>
+                <td>{$row['quantity']}</td>
+                <td>{$row['price_at_purchase']}</td>
+                <td>{$row['ticket_type']}</td>
+                <td>{$row['status']}</td>
+                </tr>
+                ";
+        }
+        ?>
+    </table>
+</div>
 </body>
 </html>
