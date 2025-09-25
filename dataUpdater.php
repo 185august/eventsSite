872ba713e3ee083150e5d41e18dad43b4cb61559
185august <augusteide98@hotmail.com>
2025-09-25 11:32:29 +0200

    Fix include statement in dataUpdater and added a createNewEvent function

--- dataUpdater.php
+++ dataUpdater.php
@@ -1,5 +1,5 @@
 <?php
-include 'dbConnection.php';
+include_once 'dbConnection.php';
 
 
 function createNewUser($name, $email, $phone)
@@ -19,6 +19,18 @@ function createNewVenue($name, $address, $capacity, $email)
     $connection = getDbConnection();
     $sql = "INSERT INTO venues (name, address, capacity, contact_info) VALUES ($1, $2, $3, $4)";
     $params = array($name, $address, $capacity, $email);
+
+    $result = pg_query_params($connection, $sql, $params);
+    if (!$result) {
+        die("Query failed: " . pg_last_error($connection));
+    }
+}
+
+function createNewEvent($title, $description, $venue_id, $start_time, $end_time, $organizer_id)
+{
+    $connection = getDbConnection();
+    $sql = "INSERT INTO events (title, description, venue_id, start_time, end_time, organizer_id) VALUES ($1, $2, $3, $4, $5, $6)";
+    $params = array($title, $description, $venue_id, $start_time, $end_time, $organizer_id);
 
     $result = pg_query_params($connection, $sql, $params);
     if (!$result) {
