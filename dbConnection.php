d0e62a1ef586802d395d9ac5ea0b435f8805269b
185august <augusteide98@hotmail.com>
2025-10-06 10:19:09 +0200

    Moved test input to `dbConnection.php` While this does not really fit here, it helps my code be more readable and reusable

--- dbConnection.php
+++ dbConnection.php
@@ -13,3 +13,10 @@ function getDbConnection()
     return $connection;
 }
 
+function testInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
