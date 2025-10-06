a83d0c1f863e084ead1e9e9d669d8576419587c0
185august <augusteide98@hotmail.com>
2025-10-06 10:20:14 +0200

    A new site for registering a new event!

--- registerNewEventForm.php
+++ registerNewEventForm.php
@@ -1,86 +1,91 @@
 <!<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register new event</title>
-</head>
-<body>
-<a href="index.php" class="button-link">Go back</a>
-<?php
-include_once 'dataFetcher.php';
-$dataFetcher = new DataFetcher();
-$venusResult = $dataFetcher->fetchVenues();
-$usersResult = $dataFetcher->fetchUsers();
-?>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    Event name: <label>
-        <input name="eventName" required type="text">
-    </label>
-    <br>
-    Event description: <label>
-        <input name="description" required type="text">
-    </label>
-    <br>
-    Start Time: <label>
-        <input name="startTime" required type="datetime-local">
-    </label>
-    <br>
-    End Time: <label>
-        <input name="endTime" required type="datetime-local">
-    </label>
-    <br>
-    Venue Name: <label>
-        <select name="venue">
-            <?php
+    <html lang="en">
 
-            while ($row = pg_fetch_assoc($venusResult)) {
-                echo "<option value='{$row['venue_id']}'>{$row['name']}</option>";
-            }
-            ?>
-        </select>
-    </label>
-    <br>
-    Organizer Name: <label>
-        <select name="organizer">
-            <?php
-            while ($row = pg_fetch_assoc($usersResult)) {
-                echo "<option value='{$row['user_id']}'>{$row['name']}</option>";
-            }
-            ?>
-        </select>
-    </label>
-    <br>
-    <input type="submit" value="Register">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Register new event</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
 
-</form>
+        <body>
+            <div class="data-container">
+                <a href="index.php" class="button-link">Go back</a>
+                <?php
+                include_once 'dataFetcher.php';
+                $dataFetcher = new DataFetcher();
+                $venusResult = $dataFetcher->fetchVenues();
+                $usersResult = $dataFetcher->fetchUsers();
+                ?>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    Event name: <label>
+                        <input name="eventName" required type="text">
+                    </label>
+                    <br>
+                    Event description: <label>
+                        <input name="description" required type="text">
+                    </label>
+                    <br>
+                    Start Time: <label>
+                        <input name="startTime" required type="datetime-local">
+                    </label>
+                    <br>
+                    End Time: <label>
+                        <input name="endTime" required type="datetime-local">
+                    </label>
+                    <br>
+                    Venue Name: <label>
+                        <select name="venue">
+                            <?php
 
-</body>
-</html>
-<?php
-include_once 'dataUpdater.php';
-$title = $description = $venue_id = $organizer_id = $start_time = $end_time = "";
+                            while ($row = pg_fetch_assoc($venusResult)) {
+                                echo "<option value='{$row['venue_id']}'>{$row['name']}</option>";
+                            }
+                            ?>
+                        </select>
+                    </label>
+                    <br>
+                    Organizer Name: <label>
+                        <select name="organizer">
+                            <?php
+                            while ($row = pg_fetch_assoc($usersResult)) {
+                                echo "<option value='{$row['user_id']}'>{$row['name']}</option>";
+                            }
+                            ?>
+                        </select>
+                    </label>
+                    <br>
+                    <input type="submit" value="Register">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $title = test_input($_POST["eventName"]);
-    $description = test_input($_POST["description"]);
-    $venue_id = test_input($_POST["venue"]);
-    $organizer_id = test_input($_POST["organizer"]);
-    $start_time = test_input($_POST["startTime"]);
-    $end_time = test_input($_POST["endTime"]);
-    
+                </form>
+            </div>
+        </body>
 
-    createNewEvent($title, $description, $venue_id, $start_time, $end_time, $organizer_id);
-    // The database handles updating the event_participants table through triggers.
-    header("Location: index.php");
-}
+    </html>
+    <?php
+    include_once 'dataUpdater.php';
+    $title = $description = $venue_id = $organizer_id = $start_time = $end_time = "";
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $title = test_input($_POST["eventName"]);
+        $description = test_input($_POST["description"]);
+        $venue_id = test_input($_POST["venue"]);
+        $organizer_id = test_input($_POST["organizer"]);
+        $start_time = test_input($_POST["startTime"]);
+        $end_time = test_input($_POST["endTime"]);
 
-?>
+
+        createNewEvent($title, $description, $venue_id, $start_time, $end_time, $organizer_id);
+        // The database handles updating the event_participants table through triggers.
+        header("Location: index.php");
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    ?>
